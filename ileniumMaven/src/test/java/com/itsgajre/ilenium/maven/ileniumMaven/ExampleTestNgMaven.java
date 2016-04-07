432e2147305cd0260f33095451cd00f46959bc91
Srinivas Gajre <gajre@itsgajre.com>
2016-04-07 15:15:44 +0100

    New Project

---
+++ ileniumMaven/src/test/java/com/itsgajre/ilenium/maven/ileniumMaven/ExampleTestNgMaven.java
@@ -0,0 +1,10 @@
+package com.itsgajre.ilenium.maven.ileniumMaven;
+import org.testng.annotations.Test;
+
+public class ExampleTestNgMaven {
+
+	@Test
+	public void testClass() {		
+		System.out.println("TestNG example");
+	}
+}
---
+++ ileniumMaven/src/test/java/com/itsgajre/ilenium/maven/ileniumMaven/ExampleTestNgMavenTwo.java
@@ -0,0 +1,12 @@
+package com.itsgajre.ilenium.maven.ileniumMaven;
+
+import org.testng.annotations.Test;
+
+public class ExampleTestNgMavenTwo {
+	
+	@Test
+	public void testClassTwo(){
+		System.out.println("This is 2nd example of Maven-TestNg");
+	}
+
+}
